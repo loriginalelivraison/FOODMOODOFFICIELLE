327f4ce9e58684d9ceac26d6232d29dd146e6f8d
sameh <samehbouzid20@outlook.com>
2026-05-07 16:11:13 +0200

    k

--- livreur-pro-back/config/settings.py
+++ livreur-pro-back/config/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os 
 import dj_database_url
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=365),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=365),
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
