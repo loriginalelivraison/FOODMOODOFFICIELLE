b6441a6dab22e1f036822899fc9b9d053bbecb4d
sameh <samehbouzid20@outlook.com>
2026-05-07 15:44:41 +0200

    o

--- livreur-pro/src/pages/TrackingMap.jsx
+++ livreur-pro/src/pages/TrackingMap.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 export default function TrackingMap({ courier }) {
   const latitude = Number(courier.latitude);
