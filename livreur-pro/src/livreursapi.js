d29d632bb1e6691528acde50b3684c24a8888b1a
sameh <samehbouzid20@outlook.com>
2026-05-07 12:25:58 +0200

    fix backend api url

--- livreur-pro/src/livreursapi.js
+++ livreur-pro/src/livreursapi.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "import.meta.env.VITE_API_BASE_URL";
+const API_BASE_URL = "https://livreur-pro-back.fly.dev/api";
 
 export async function getLivreurs() {
   const response = await fetch(`${API_BASE_URL}/livreurs/?format=json`);
