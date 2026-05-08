b3b86116daaa9c9487fc5c0ef4188b4e8363313e
sameh <samehbouzid20@outlook.com>
2026-05-08 20:34:21 +0200

    k

--- livreur-pro-back/deliveries/models.py
+++ livreur-pro-back/deliveries/models.py
@@ -73,3 +73,31 @@ class DemandeLivraison(models.Model):
 
     def __str__(self):
         return f"Demande {self.id} - {self.client_nom}"
+
+#model de table des commentaires des clients sur livreure s
+class CommentaireLivreur(models.Model):
+    livreur = models.ForeignKey(
+        Livreur,
+        on_delete=models.CASCADE,
+        related_name="commentaires"
+    )
+    nom_client = models.CharField(max_length=100, blank=True)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Commentaire pour {self.livreur.nom}"
+
+#table client  
+class Client(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="client_profile"
+    )
+    nom = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=20, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nom
