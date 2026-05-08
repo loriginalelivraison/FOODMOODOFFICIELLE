2f48ac24efd9298a6af813e177195dbcacab8f25
sameh <samehbouzid20@outlook.com>
2026-05-08 20:34:21 +0200

    k

--- livreur-pro-back/deliveries/urls.py
+++ livreur-pro-back/deliveries/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LivreurViewSet, DemandeLivraisonViewSet, register_livreur
+from .views import LivreurViewSet, DemandeLivraisonViewSet, register_livreur, CommentaireLivreurViewSet, ClientViewSet, register_client
 
 router = DefaultRouter()
 router.register("livreurs", LivreurViewSet)
 router.register("demandes", DemandeLivraisonViewSet)
+router.register(r"commentaires-livreurs", CommentaireLivreurViewSet, basename="commentaires-livreurs")
+router.register(r"clients", ClientViewSet, basename="client")
 
 urlpatterns = [
+    path("clients/register/", register_client),
     path("livreurs/register/", register_livreur),
-    path("", include(router.urls))
+    path("", include(router.urls)),
+   
    
 ]
