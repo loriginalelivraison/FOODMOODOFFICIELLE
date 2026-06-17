58e03a663408ebcc34efa023616e7ed403d7bf1c
sameh <samehbouzid20@outlook.com>
2026-06-17 21:27:45 +0200

    k

--- flutter/foodmood_app/android/app/build.gradle.kts
+++ flutter/foodmood_app/android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.foodmood_app"
+    namespace = "fr.winrak.app"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -31,7 +31,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.winrak.app"
+        applicationId = "fr.winrak.app"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = flutter.versionCode
