b3d957008ed2f19ded3d5215889b9e604873a6cf
sameh <samehbouzid20@outlook.com>
2026-06-17 19:10:28 +0200

    k

--- livreur-pro-back/config/settings.py
+++ livreur-pro-back/config/settings.py
@@ -152,6 +152,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://foodmoodofficielle-3f1e.vercel.app",
+    "https://winrak.fr",
+    "https://www.winrak.fr",
 ]
 
 REST_FRAMEWORK = {
