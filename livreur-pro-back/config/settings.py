7e89f13a15375a1a8c21c7b7004863295caaec6f
sameh <samehbouzid20@outlook.com>
2026-07-09 22:10:26 +0200

    Add WinRak domains to CORS

--- livreur-pro-back/config/settings.py
+++ livreur-pro-back/config/settings.py
@@ -153,7 +153,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "https://foodmoodofficielle-3f1e.vercel.app",
     "https://winrak.fr",
-    "https://www.winrak.fr",
+    "https://www.winrak.fr",#
 ]
 
 REST_FRAMEWORK = {
